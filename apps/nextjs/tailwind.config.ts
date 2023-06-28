9b11a809a86a4fc1e22aad474aae75310db0f6d3
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2023-06-28 19:28:04 +0700

    website temp

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,7 +1,16 @@
-import baseConfig from "@empathattic/tailwind-config";
 import type { Config } from "tailwindcss";
+
+import baseConfig from "@empathattic/tailwind-config";
 
 export default {
   content: ["./src/**/*.tsx"],
   presets: [baseConfig],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        heading: ["var(--font-raleway)"],
+      },
+    },
+  },
 } satisfies Config;
